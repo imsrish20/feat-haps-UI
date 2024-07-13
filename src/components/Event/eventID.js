ea169f504a65c0eb96df319c5f2a6a57466fa79f
imsrish20 <imsrish20@gmail.com>
2024-07-14 01:28:23 +0530

    slide show working on event details page and new eventcard is started

--- src/components/Event/eventID.js
+++ src/components/Event/eventID.js
@@ -1,8 +1,18 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { useParams } from 'react-router-dom';
-import { Typography, CssBaseline, AppBar, Toolbar, useScrollTrigger, Slide } from '@mui/material';
-
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import PhotoGallery from "./gallery"
+import {
+  Typography,
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  useScrollTrigger,
+  Slide,
+} from "@mui/material";
+import Masonry from '@mui/lab/Masonry';
+import { URL } from "../Constants";
 function HideOnScroll(props) {
   const { children, window } = props;
   const trigger = useScrollTrigger({ target: window ? window() : undefined });
@@ -23,7 +33,7 @@ const HideAppBar = (props) => {
   return (
     <React.Fragment>
       <HideOnScroll {...props}>
-        <AppBar style={{backgroundColor: "orange"}}>
+        <AppBar style={{ backgroundColor: "orange" }}>
           <Toolbar>
             <Typography variant="h6" component="div">
               {props.children}
@@ -35,23 +45,57 @@ const HideAppBar = (props) => {
   );
 };
 
-const EventDetails = (event) => {
-  console.log(event.name);
+const EventDetails = () => {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
 
+  useEffect(() => {
+    axios
+      .get(`${URL}/events`)
+      .then((response) => {
+        if (response.status === 200) {
+          setEvents(response.data.events);
+        } else {
+          console.error("Something went wrong!");
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching events:", error);
+        setLoading(false);
+      });
+  }, []);
 
+  const event = events.find((event) => event._id === id);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!event) {
+    return <div>Event not found</div>;
+  }
   return (
     <React.Fragment>
       <CssBaseline />
-      <HideAppBar>
-        Event Details for ID: {event.name}
-      </HideAppBar>
+      <HideAppBar>{event.name}</HideAppBar>
       <div style={{ padding: 16, marginTop: 64 }}>
         <Typography variant="body1">
-          Here is some detailed information about the event with ID {event.id}...
+          
         </Typography>
       </div>
+      <PhotoGallery></PhotoGallery>
+
+      <div style={{ display: 'flex', marginLeft: '30px' }}>
+
+      </div>
+
+
+
+
     </React.Fragment>
   );
-}
+};
 
 export default EventDetails;
---
+++ src/components/Event/gallery.js
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import { Grid, IconButton, Paper, Box } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import pic1 from "../PhotoGallery/pic1.jpg";
+import pic2 from "../PhotoGallery/pic2.jpg";
+import pic3 from "../PhotoGallery/pic3.jpg";
+import pic4 from "../PhotoGallery/pic4.jpg";
+import pic5 from "../PhotoGallery/pic5.png";
+
+const photos = [pic1, pic2, pic3, pic4, pic5]; // Ensure at least 5 images for layout consistency
+
+function PhotoGallery() {
+  const [current, setCurrent] = useState(0);
+  const totalPhotos = photos.length;
+
+  const next = () => {
+    setCurrent((current + 1) % totalPhotos);
+  };
+
+  const previous = () => {
+    setCurrent((current - 1 + totalPhotos) % totalPhotos);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prevCurrent) => (prevCurrent + 1) % totalPhotos);
+    }, 4000); // Automatically switch every 4 seconds
+
+    return () => clearInterval(interval);
+  }, [totalPhotos]);
+
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+        marginTop: "-150px"
+      }}
+    >
+      <IconButton
+        onClick={previous}
+        sx={{
+          position: "absolute",
+          left: 16,
+          top: "50%",
+          transform: "translateY(-50%)",
+          zIndex: 1,
+        }}
+      >
+        <ArrowBackIosIcon />
+      </IconButton>
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        justifyContent="center"
+        sx={{ maxWidth: "100%", overflow: "hidden" }}
+      >
+        {[0, 1, 2].map((index) => {
+          const photoIndex = (current + index) % totalPhotos;
+          return (
+            <Grid
+              item
+              key={index}
+              sx={{
+                width: index === 1 ? "290px" : "270px",
+                height: index === 1 ? "410px" : "380px",
+                padding: "8px",
+                cursor: "pointer",
+                "&:hover": { transform: "scale(1.05)" },
+              }}
+            >
+              <Paper
+                elevation={3}
+                sx={{
+                  width: "100%",
+                  height: "300px",
+                  cursor: "pointer",
+                }}
+              >
+                <img
+                  src={photos[photoIndex]}
+                  alt={`pic-${photoIndex}`}
+                  style={{
+                    width: "100%",
+                    height: "380px",
+                    objectFit: "cover",
+                    borderRadius: "8px",
+                  }}
+                />
+              </Paper>
+            </Grid>
+          );
+        })}
+      </Grid>
+      <IconButton
+        onClick={next}
+        sx={{
+          position: "absolute",
+          right: 16,
+          top: "50%",
+          transform: "translateY(-50%)",
+          zIndex: 1,
+        }}
+      >
+        <ArrowForwardIosIcon />
+      </IconButton>
+    </Box>
+  );
+}
+
+export default PhotoGallery;
